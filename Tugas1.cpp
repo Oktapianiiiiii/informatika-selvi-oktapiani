aa4bf25be514f9c764af8d65ec479559dc406b2c
Oktapianiiiiii <selvioktaviani712@gmail.com>
2025-08-05 13:01:19 +0700

    Add files via upload

---
+++ Tugas1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Selvi oktapiani" << endl;
+    cout << "Cianjur Pasirkuda 18 Oktober 2009"<< endl;
+    cout << "10 IPA" << endl;
+    cout << "MAS ALMANSHURIYAH" << endl;
+    
+    return 0;
+}   
